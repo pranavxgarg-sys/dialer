5d6e30835cd76a474ff333968ed362fbd2caac06
pranavxgarg-sys <pranavxgarg@gmail.com>
2026-04-28 13:36:16 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,20 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("dialer").then(cache => {
-      return cache.addAll(["./", "./index.html"]);
-    })
+const CACHE_NAME = "dialer-v1";
+
+const urlsToCache = [
+  "/dialer/",
+  "/dialer/index.html"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
